ba7114692d3c5fc555f661f87dc3409bebacdef9
pythexcel <pythexcel@excellencetechnologies.in>
2021-05-14 16:10:59 +0200

    updated

--- server/node_modules/cockatiel/dist/esm/backoff/Backoff.test.d.ts
+++
@@ -1,2 +0,0 @@
-import { IBackoff } from './Backoff';
-export declare const expectDurations: <T>(backoff: IBackoff<T> | undefined, expected: readonly (number | undefined)[], context?: T | undefined) => void;
